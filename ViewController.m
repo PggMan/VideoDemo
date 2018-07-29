cd5b36d4554f0b1c2cb6ecea75e179b83ab6ba4b
PggMan <78715199@qq.com>
2018-07-29 16:48:53 +0800

    yinduasan
    摄像头切换

--- ViewController.m
+++ ViewController.m
@@ -11,10 +11,13 @@
 @interface ViewController ()<AVCaptureVideoDataOutputSampleBufferDelegate>
 /** 必须进行强引用*/
 @property (nonatomic ,strong)  AVCaptureSession *session;
+@property (nonatomic ,weak)  AVCaptureDeviceInput *input;
+@property (nonatomic ,weak)  AVCaptureOutput *output;
+@property (nonatomic ,assign) AVCaptureDevicePosition curPosition;
+
 @property (nonatomic ,weak)  AVCaptureVideoPreviewLayer *previewLayer;
 @property (nonatomic ,weak) UIImageView *imageView;
 @end
-
 @implementation ViewController
 
 - (UIImageView *)imageView{
@@ -41,6 +44,7 @@
     [session startRunning];
     
 }
+#pragma mark - 采集初始信息
 - (AVCaptureSession *)addCaptureSession{
     
     AVCaptureSession *session = [[AVCaptureSession alloc] init];
@@ -57,7 +61,9 @@
 - (AVCaptureDevice *)addCaptureDevice:(AVCaptureSession *)session{
     
     //2 创建Device
-    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    _curPosition = AVCaptureDevicePositionFront;
+    
+    AVCaptureDevice *device = [self cameroWithPosition:_curPosition];
     
     if ([device lockForConfiguration:nil]) {
         //自动闪光灯，
@@ -68,12 +74,12 @@
         // 帧率 1秒10帧
         device.activeVideoMinFrameDuration = CMTimeMake(1, 10);
         
-        
         //自动白平衡,
         if ([device isWhiteBalanceModeSupported:AVCaptureWhiteBalanceModeContinuousAutoWhiteBalance]) {
             [device setWhiteBalanceMode:AVCaptureWhiteBalanceModeContinuousAutoWhiteBalance];
         }
         [device unlockForConfiguration];
+      
     }
     
     return device;
@@ -86,6 +92,7 @@
     AVCaptureDeviceInput *input =  [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
     
     if(input) {
+         _input = input;
         [_session addInput:input];
     } else {
         NSLog(@"%@", error);
@@ -114,6 +121,8 @@
                                     nil];
     output.videoSettings = setcapSettings;
     //4.2 获取输入与输出之间的连接 设置
+    _output = output;
+    
     [self setConnection:output];
     
 }
@@ -122,6 +131,14 @@
     // 桥接
     AVCaptureConnection *connection = [output connectionWithMediaType:AVMediaTypeVideo];
     connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    
+    // 解决前置摄像头左右成像问题:(当前是前置摄像头 则启动镜子反射效果)
+    if (_curPosition == AVCaptureDevicePositionUnspecified || _curPosition == AVCaptureDevicePositionFront) {
+        connection.videoMirrored = YES;
+    } else {
+        connection.videoMirrored = NO;
+    }
+
 }
 
 
@@ -134,6 +151,7 @@
     
 }
 
+#pragma mark - 图片处理
 // 代理
 - (void)captureOutput:(AVCaptureOutput *)output didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
     // captureSession 会话如果没有强引用，这里不会得到执行
@@ -150,8 +168,12 @@
         image = [self imageFromSampleBuffer:imageBuffer];
     }
     
+    
+    
     dispatch_async(dispatch_get_main_queue(), ^{
-        self.imageView.image = image;
+        
+      self.imageView.image = image;
+        
     });
 }
 // iOS9.0 图片处理
@@ -169,6 +191,7 @@
     size_t height = CVPixelBufferGetHeight(imageBuffer);
     // 创建一个依赖于设备的RGB颜色空间
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    
     // 用抽样缓存的数据创建一个位图格式的图形上下文（graphics context）对象
     CGContextRef context = CGBitmapContextCreate(baseAddress, width, height, 8, bytesPerRow, colorSpace, kCGBitmapByteOrder32Little | kCGImageAlphaPremultipliedFirst);
     // 根据这个位图context中的像素数据创建一个Quartz image对象
@@ -179,10 +202,86 @@
     CGContextRelease(context); CGColorSpaceRelease(colorSpace);
     // 用Quartz image创建一个UIImage对象image
     UIImage *image = [UIImage imageWithCGImage:quartzImage scale:1 orientation:UIImageOrientationUp];
+    
     // 释放Quartz image对象
     CGImageRelease(quartzImage);
     return (image);
 }
+
+
+#pragma mark - 摄像头切换
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    
+    if (self.input) {
+        //1 若当前是前置摄像头，则需新设后摄像头，反之，亦如此。
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            
+            AVCaptureDevicePosition position  = (self.input.device.position == AVCaptureDevicePositionFront) ? AVCaptureDevicePositionBack :AVCaptureDevicePositionFront;
+            _curPosition = position;
+            
+            // 获取摄像头设备(前置 或者 后置 依据传入的 AVCaptureDevicePosition 而定)
+            AVCaptureDevice *device = [self cameroWithPosition:position];
+            
+  
+            NSError *error;
+            AVCaptureDeviceInput *newInput =  [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+            
+            if(newInput) {
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    CATransition *rotaionAnim = [CATransition animation];
+                    rotaionAnim.type = @"oglFlip";
+                    rotaionAnim.subtype = @"fromLeft";
+                    rotaionAnim.duration = 0.5;
+                    [self.view.layer addAnimation:rotaionAnim forKey:nil];
+                    
+                    
+                    [self.session beginConfiguration];
+                    // 重设输入源
+                    [self.session removeInput:self.input];
+                    self.input = newInput;
+                    [self.session addInput:newInput];
+                    
+                    // 重设输出源
+                    [self.session removeOutput:self.output];
+                    [self addCaptureDeviceOutput:device];
+                    
+                    [self.session commitConfiguration];
+                });
+                
+            } else {
+                NSLog(@"%@", error);
+                return;
+            }
+        });
+    }
+    
+    
+}
+
+- (AVCaptureDevice *)cameroWithPosition:(AVCaptureDevicePosition)position{
+    
+   
+        if (@available(iOS 10.0, *)) {
+            AVCaptureDeviceDiscoverySession *dissession = [AVCaptureDeviceDiscoverySession discoverySessionWithDeviceTypes:@[AVCaptureDeviceTypeBuiltInDuoCamera,AVCaptureDeviceTypeBuiltInTelephotoCamera,AVCaptureDeviceTypeBuiltInWideAngleCamera] mediaType:AVMediaTypeVideo position:position];
+            for (AVCaptureDevice *device in dissession.devices) {
+                if ([device position] == position ) {
+                    return device;
+                }
+            }
+        } else {
+            NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+            for (AVCaptureDevice *device in devices) {
+                if ([device position] == position) {
+                    return device;
+                }
+            }
+        }
+    return nil;
+}
+
 
 
 @end
